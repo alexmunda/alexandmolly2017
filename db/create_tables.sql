bfd4777387acb56bac1d4b75e069d3b831cdca43
Alex Munda <alex.munda3@gmail.com>
2016-09-16 13:28:56 -0500

    Fix db create script format

--- db/create_tables.sql
+++ db/create_tables.sql
@@ -1,13 +1,13 @@
-create table guests (
+CREATE TABLE groups (
+  group_id serial PRIMARY KEY,
+  date_created timestamp NOT NULL
+);
+
+CREATE TABLE guests (
   guest_id integer PRIMARY KEY,
   first_name text NOT NULL,
   last_name text NOT NULL,
   attending boolean NOT NULL,
   date_created timestamp NOT NULL,
   group_id integer NOT NULL REFERENCES groups(group_id)
-)
-
-create table groups (
-  group_id integer PRIMARY KEY,
-  date_created timestamp NOT NULL
-)
+);
