000ba2337a4f8cfde0aeafa97cd907d1bb8034b8
Alex Munda <alex.munda3@gmail.com>
2017-03-05 21:42:39 -0600

    Nav arrows

--- src/client/elm/Photos/Main.elm
+++ src/client/elm/Photos/Main.elm
@@ -196,38 +196,34 @@ view model =
                         [ class "gallery" ]
                         [ div [ class "gallery-closeTarget", onClick PhotoClosed ] []
                         , div [ class "col-lg-8 col-lg-offset-2 col-md-8 col-md-offset-2 gallery-media" ]
-                            [ img [ class "media-image img img-responsive", src selectedPhoto.url ] []
+                            [ span []
+                                [ i
+                                    ([ class "fa fa-chevron-left left-arrow arrow"
+                                     ]
+                                        ++ if selectedPhoto.index == 1 then
+                                            [ class "hidden" ]
+                                           else
+                                            [ onClick PreviousPhoto
+                                            ]
+                                    )
+                                    []
+                                ]
+                            , img [ class "media-image img img-responsive", src selectedPhoto.url ] []
+                            , span []
+                                [ i
+                                    ([ class "fa fa-chevron-right right-arrow arrow"
+                                     ]
+                                        ++ if selectedPhoto.index == List.length model.photos then
+                                            [ class "hidden" ]
+                                           else
+                                            [ onClick NextPhoto
+                                            ]
+                                    )
+                                    []
+                                ]
                             ]
                         ]
                     ]
 
-                --     [ class "gallery" ]
-                --     [ div [ class "gallery-closeTarget" ] []
-                --     , div [ class "gallery-content", style [ ( "width", "800px" ), ( "min-height", "0px" ) ] ]
-                --         [ button [ class "modal-btn modal-close", onClick PhotoClosed ]
-                --             [ span [ class "icon icon--close icon--large" ]
-                --                 [ i [ class "fa fa-times" ] []
-                --                 ]
-                --             ]
-                --         , div [ class "gallery-media" ]
-                --             [ img [ class "media-image img img-responsive", src selectedPhoto.url ] []
-                --             ]
-                --         , div [ class "galleryNav galleryNav--prev" ]
-                --             [ span [ class "galleryNav-handle galleryNav-handle--prev" ]
-                --                 [ span [ class "icon icon--large icon--left", onClick PreviousPhoto ]
-                --                     [ i [ class "fa fa-arrow-left" ] []
-                --                     ]
-                --                 ]
-                --             ]
-                --         , div [ class "galleryNav galleryNav--next" ]
-                --             [ span [ class "galleryNav-handle galleryNav-handle--next" ]
-                --                 [ span [ class "icon icon--large", onClick NextPhoto ]
-                --                     [ i [ class "fa fa-arrow-right" ] []
-                --                     ]
-                --                 ]
-                --             ]
-                --         ]
-                --     ]
-                -- ]
                 Nothing ->
                     [ span [] [] ]
